bd37b65ffe258ead08fe24b5263c533aabef1e53
StefanysFerreira <stefanysferreira18@gmail.com>
2023-06-22 20:34:01 -0300

    melhorias #6

--- test/unit/mediaCA.js
+++ test/unit/mediaCA.js
@@ -1,16 +1,16 @@
 function mediaCA(media) {
     if (media >= 9.0 && media <= 10) {
-        return 'SS - Superior';
+        return 'SS';
     } else if (media >= 7.0 && media < 9.0) {
-        return 'MS - Médio Superior';
+        return 'MS';
     } else if (media >= 5.0 && media < 7.0) {
-        return 'MM - Médio';
+        return 'MM';
     } else if (media >= 3.0 && media < 5.0) {
-        return 'MI - Médio Inferior';
+        return 'MI';
     } else if (media >= 0.1 && media < 3.0) {
-        return 'II - Inferior';
+        return 'II';
     } else if (media === 0.0) {
-        return 'SR - Sem rendimento';
+        return 'SR';
     } else {
         return 'Valor de média inválido';
     }
